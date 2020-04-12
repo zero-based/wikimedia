8db3ce517389db9d6061a7353f22009de9abc745
MonicaAtef <monicaatef46@gmail.com>
2020-04-13 00:33:32 +0200

    Add Commits View & Controller

---
+++ Wikimedia/Controllers/CommitsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Wikimedia.Models;
+
+namespace Wikimedia.Controllers
+{
+    public class CommitsController : Controller
+    {
+        // GET: Commit
+        public ActionResult Index()
+        {
+            var commits = getCommits();
+            return View(commits);
+        }
+
+        public IEnumerable<Commit> getCommits()
+        {
+            return new List<Commit>
+            {
+                new Commit
+                {
+                    Author =new UserAccount {Username = "YoussefRaafatNassry"}, Date = DateTime.Now, Message = "Add Topics View & Controller"
+                },
+                new Commit { Author=new UserAccount {Username = "MonicaTanios"}, Date = DateTime.Now, Message = "Add SignIn/Up Views & Controllers"}
+            };
+        }
+    }
+}
