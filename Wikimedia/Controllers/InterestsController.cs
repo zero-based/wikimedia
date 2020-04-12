e287079dc9c6ff5b6bafaa2a45090419bdc1859b
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2020-04-13 01:37:20 +0200

    Add Interests View & Controller

---
+++ Wikimedia/Controllers/InterestsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Wikimedia.Models;
+
+namespace Wikimedia.Controllers
+{
+    public class InterestsController : Controller
+    {
+        // GET: Interests
+        public ActionResult Interests()
+        {
+            var interests = new List<Interest>
+            {
+                new Interest() {Id = 1, Name = "Interest 1"},
+                new Interest() {Id = 2, Name = "Interest 2"},
+                new Interest() {Id = 3, Name = "Interest 3"},
+                new Interest() {Id = 4, Name = "Interest 4"},
+                new Interest() {Id = 5, Name = "Interest 5"}
+            };
+
+            return View(interests);
+        }
+    }
+}
