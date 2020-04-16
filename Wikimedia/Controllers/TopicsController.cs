7df22a88f1d58f5a632d573229f13b256b8536d5
MichelineMedhat <MichelineMedhat@gmail.com>
2020-04-16 15:38:01 +0200

    Add Topics View

--- Wikimedia/Controllers/TopicsController.cs
+++ Wikimedia/Controllers/TopicsController.cs
@@ -1,34 +1,49 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using Core.Models;
 
 namespace Web.Controllers
 {
     public class TopicsController : Controller
     {
-        [Route("topics/{name}")]
-        public ActionResult Index(string name)
+        private static readonly Topic CachedTopic = new Topic
         {
-            var tmpTopic = new Topic
-            {
-                Name = name,
-                FilePath = "/",
-                Creator = new UserAccount { Username = "zero-based" },
-                UpVotes = 100,
-                DownVotes = 5,
-                Body = "**Lorem ipsum** dolor sit amet, consectetur adipiscing elit," +
-                       "sed do eiusmod tempor _incididunt_ ut labore et dolore magna " +
-                       "aliqua. Ut enim ad minim veniam, quis nostrud exercitation " +
-                       "ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
-                       "Duis aute irure dolor in reprehenderit in voluptate velit " +
-                       "esse cillum dolore eu fugiat nulla pariatur. Excepteur sint " +
-                       "occaecat cupidatat non proident, sunt in culpa qui officia " +
-                       "deserunt mollit anim id est laborum.\n\n" +
-                       " - List item\n" +
-                       " - List item\n" +
-                       " - List item\n"
-            };
+            Name = "CachedTopic",
+            FilePath = "/",
+            Creator = new UserAccount {Username = "zero-based"},
+            UpVotes = 100,
+            DownVotes = 5,
+            Body = "**Lorem ipsum** dolor sit amet, consectetur adipiscing elit," +
+                   "sed do eiusmod tempor _incididunt_ ut labore et dolore magna "
+        };
 
-            return View(tmpTopic);
+        private static readonly Topic UncachedTopic = new Topic
+        {
+            Name = "UncachedTopic",
+            FilePath = "/",
+            Creator = new UserAccount {Username = "zero-based"},
+            UpVotes = 100,
+            DownVotes = 5,
+            Body = "**Lorem ipsum** dolor sit amet, consectetur adipiscing elit," +
+                   "sed do eiusmod tempor _incididunt_ ut labore et dolore magna "
+        };
+
+        private readonly Dictionary<string, Topic> _pool = new Dictionary<string, Topic>
+        {
+            {"CachedTopic", CachedTopic},
+        };
+
+        public ActionResult Index()
+        {
+            return View(_pool.Values);
+        }
+
+        [Route("topics/{name}")]
+        public ActionResult TopicByName(string name)
+        {
+            if (_pool.ContainsKey(name)) return View(_pool[name]);
+
+            return View(UncachedTopic);
         }
     }
 }
