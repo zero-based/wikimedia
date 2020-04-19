08168d68764394cccb896c5ec36a96a228808c81
MonicaTanios <monicatanios@gmail.com>
2020-04-19 22:43:29 +0200

    Add User View & Controller

---
+++ Web/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Core.Models;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Web.Util;
+
+namespace Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UserProfile()
+        {
+            return View(AuthController.LoggedInUser);
+        }
+
+        [HttpPost]
+        public ActionResult Update(UserAccount userAccount)
+        {
+            if (!ModelState.IsValid)
+                return View("UserProfile", userAccount);
+            var userAccountRepository = new UserAccountRepository();
+            userAccount.Password = Security.ToHash(userAccount.Password);
+            userAccountRepository.Update(userAccount);
+            return RedirectToAction("Index", "Interests");
+        }
+    }
+}
